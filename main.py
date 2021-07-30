9ca652aefec6e0dbb5b5f13ffd116c325b4435c3
AlamReyes <86863022+AlamReyes@users.noreply.github.com>
2021-07-29 22:37:21 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+First_name = input("Write your first name: ")
+Middle_name = input("Write your second name: ")
+Last_name = input("Write your last name: ")
+
+print("Your initials are " + First_name[0] + Middle_name[0] + Last_name[0])
+
+
+print("Your company has a product with this lot number: 037-00901-00027")
+
+lotNumber = str("0370090100027")
+
+print("The country code is " + lotNumber[0:3])
+print("The product code is " + lotNumber[3:8])
+print("The bach code is " + lotNumber[8:13])
+
